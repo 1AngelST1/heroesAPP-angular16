218c328558b48bca0816fda71e3d7a057c54b05f
1AngelST1 <angel.sarmiento0312@gmail.com>
2025-10-26 19:36:30 -0600

    se crearon las carptes de component este contiene los archivos de los listados de los herores, pipes el cargado de las imagenes

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -5,8 +5,46 @@ import { Heroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
-  ]
+  styles: [`
+    .heroes-container {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 20px;
+      padding: 20px;
+    }
+
+    .heroe-item {
+      flex: 0 0 20%;
+      min-width: 200px;
+      max-width: 300px;
+    }
+
+    /* Responsive: pantallas grandes */
+    @media (min-width: 1280px) {
+      .heroe-item {
+        flex: 0 0 15%;
+      }
+    }
+
+    /* Responsive: pantallas medianas */
+    @media (max-width: 960px) {
+      .heroe-item {
+        flex: 0 0 30%;
+      }
+    }
+
+    /* Responsive: pantallas pequeñas */
+    @media (max-width: 600px) {
+      .heroes-container {
+        flex-direction: column;
+      }
+      .heroe-item {
+        flex: 0 0 100%;
+        max-width: 100%;
+      }
+    }
+  `]
 })
 
 export class ListadoComponent implements OnInit {
